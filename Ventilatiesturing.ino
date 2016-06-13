15906c5c07961e92acfdf15a246ba8399fae5415
Wespennest <bernd.schotanus@gmail.com>
2016-06-13 22:14:41 +0000

    base file
    base file, it works... but it's messy. needs some reworking

---
+++ Ventilatiesturing.ino
@@ -0,0 +1,167 @@
+
+#include <dht11.h>
+// 
+//   FILE:  dht11_test1.pde
+// PURPOSE: DHT11 library test sketch for Arduino
+//
+
+//Celsius to Fahrenheit conversion
+double Fahrenheit(double celsius)
+{
+  return 1.8 * celsius + 32;
+}
+
+// fast integer version with rounding
+//int Celcius2Fahrenheit(int celcius)
+//{
+//  return (celsius * 18 + 5)/10 + 32;
+//}
+
+
+//Celsius to Kelvin conversion
+double Kelvin(double celsius)
+{
+  return celsius + 273.15;
+}
+
+// dewPoint function NOAA
+// reference (1) : http://wahiduddin.net/calc/density_algorithms.htm
+// reference (2) : http://www.colorado.edu/geography/weather_station/Geog_site/about.htm
+//
+double dewPoint(double celsius, double humidity)
+{
+  // (1) Saturation Vapor Pressure = ESGG(T)
+  double RATIO = 373.15 / (273.15 + celsius);
+  double RHS = -7.90298 * (RATIO - 1);
+  RHS += 5.02808 * log10(RATIO);
+  RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1/RATIO ))) - 1) ;
+  RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1) ;
+  RHS += log10(1013.246);
+
+        // factor -3 is to adjust units - Vapor Pressure SVP * humidity
+  double VP = pow(10, RHS - 3) * humidity;
+
+        // (2) DEWPOINT = F(Vapor Pressure)
+  double T = log(VP/0.61078);   // temp var
+  return (241.88 * T) / (17.558 - T);
+}
+
+// delta max = 0.6544 wrt dewPoint()
+// 6.9 x faster than dewPoint()
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double dewPointFast(double celsius, double humidity)
+{
+  double a = 17.271;
+  double b = 237.7;
+  double temp = (a * celsius) / (b + celsius) + log(humidity*0.01);
+  double Td = (b * temp) / (a - temp);
+  return Td;
+}
+
+dht11 DHT11;
+
+#define DHT11PIN 5
+
+/*
+  LiquidCrystal Library - Autoscroll
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch demonstrates the use of the autoscroll()
+ and noAutoscroll() functions to make new text scroll or not.
+
+ The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalAutoscroll
+
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(2, 3, 6,7, 8, 9);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(32, 1);
+  // initialize the pushbutton pin as an input:
+  pinMode(5, INPUT);
+  pinMode(4, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // set the cursor to (0,0):
+  lcd.setCursor(0, 0);
+  //int rgButtonState[] = {};  
+  // print from 0 to 9:
+  //for (int thisChar = 0; thisChar < 10; thisChar++) {
+  //  lcd.print(thisChar);
+  //  delay(500);
+  //}
+
+  // set the cursor to (16,1):
+  lcd.setCursor(0,0);
+  // set the display to automatically scroll:
+  
+  
+  for(;;)
+  {
+    DHT11.read(DHT11PIN);
+      
+      lcd.setCursor(0,0);
+      lcd.print("dewpoint:");
+      lcd.print(dewPoint(DHT11.temperature, DHT11.humidity));
+      
+      lcd.print(' ');
+      
+      lcd.print((float)DHT11.humidity);
+      if ((float)DHT11.humidity >60)
+      {
+        digitalWrite(13,HIGH);
+      }
+      else
+      {
+        digitalWrite(13, LOW);
+      }
+      delay(1000);
+      lcd.clear();
+      lcd.print((float)DHT11.temperature);
+      delay(1000);
+  }
+      
+    
+  // turn off automatic scrolling
+  lcd.noAutoscroll();
+
+  // clear screen for the next loop:
+  lcd.clear();
+}
+
+
+
